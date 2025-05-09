ecf0e57d4757abff1681016b3ea4a105eff64e12
riccardo-guido <guidoriccardo02@gmail.com>
2025-05-09 16:40:37 +0200

    app-modified-with-clock-printend-in-terminal

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello Boolean");
+const getCurrentTime = () => {
+  const now = new Date();
+  const hours = now.getHours() < 10 ? "0" + now.getHours() : now.getHours();
+  const minutes =
+    now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
+  const seconds =
+    now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds();
+  return `Sono le ${hours}:${minutes}:${seconds}`;
+};
+
+setInterval(() => {
+  console.log(getCurrentTime());
+}, 1000);
